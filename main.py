5d1f76cc451a2e34e92d9e905d23a2c63c197a39
yogesh konduru <yogeshkonduru@gmail.com>
2022-11-03 12:13:42 +0530

    modified main file

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
-from sensor.constant import training_pipeline
 from sensor.exception import SensorException
-from sensor.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
 import os,sys
 from sensor.logger import logging
+from sensor.pipeline import training_pipeline
 from sensor.pipeline.training_pipeline import TrainPipeline
 
 if __name__ == '__main__':
-    training_pipeline= TrainPipeline()
-    training_pipeline.run_pipeline()
-
-
-# if __name__ == '__main__':
-#     # training_pipeline_config = TrainingPipelineConfig()
-#     # data_ingestion_config = DataIngestionConfig(training_pipeline_config)
-#     # print(data_ingestion_config.__dict__)
+    try:
+        training_pipeline = TrainPipeline()
+        training_pipeline.run_pipeline()
+    except Exception as e:
+        print(e)
+        logging.exception(e)
