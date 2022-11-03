de750735b0bfe43c3b7e525654d9f998feb4f7e9
yogesh konduru <yogeshkonduru@gmail.com>
2022-11-03 07:12:28 +0530

    added data validation config

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -29,3 +29,26 @@ class DataIngestionConfig:
             self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
 
 
+class DataValidationConfig:
+
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+        # the directory path for data validation
+        self.data_validation_dir:str = os.path.join(training_pipeline_config.artifact_dir,training_pipeline.DATA_VALIDATION_DIR_NAME)
+
+        #the directory path for valid and invalid
+        self.valid_data_dir:str = os.path.join(self.data_validation_dir,training_pipeline.DATA_VALIDATION_VALID_DIR)
+        self.invalid_data_dir:str = os.path.join(self.data_validation_dir,training_pipeline.DATA_VALIDATION_INVALID_DIR)
+
+        #the file path for valid train and test data
+        self.valid_train_file_path:str = os.path.join(self.valid_data_dir,training_pipeline.TRAIN_FILE_NAME)
+        self.valid_test_file_path:str = os.path.join(self.valid_data_dir,training_pipeline.TEST_FILE_NAME)
+
+        #the file path for invalid train and test data
+        self.invalid_train_file_path:str = os.path.join(self.invalid_data_dir,training_pipeline.TRAIN_FILE_NAME)
+        self.invalid_test_file_path:str = os.path.join(self.invalid_data_dir,training_pipeline.TEST_FILE_NAME)
+        
+        #the file path for the drift report
+        self.drift_report_file_path: str = os.path.join(
+            self.invalid_data_dir,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)
