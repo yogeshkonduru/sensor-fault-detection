0f41028442c6e9e5d9b572a6b9aefcad27b64348
yogesh konduru <yogeshkonduru@gmail.com>
2022-11-03 12:14:10 +0530

    modified changes to data validation

--- sensor/components/data_validation.py
+++ sensor/components/data_validation.py
@@ -49,54 +49,90 @@ class DataValidation:
             logging.info(f"Missing numerical columns: [{missing_numerical_columns}]")
             return numerical_column_present
         except Exception as e:
-            raise SensorException(e,sys)       
-
-    @staticmethod
-    def read_data(file_path) -> pd.DataFrame:
-        try :
-            return pd.read_csv(file_path)
-        except Exception as e :
-            raise SensorException(e,sys)        
-
-    def detect_dataset_drift(self):
-        try :
-            pass
-        except Exception as e :
             raise SensorException(e,sys)
 
+    @staticmethod
+    def read_data(file_path)->pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise SensorException(e,sys)
+    
+
+    def detect_dataset_drift(self,base_df,current_df,threshold=0.05)->bool:
+        try:
+            status=True
+            report ={}
+            for column in base_df.columns:
+                d1 = base_df[column]
+                d2  = current_df[column]
+                is_same_dist = ks_2samp(d1,d2)
+                if threshold<=is_same_dist.pvalue:
+                    is_found=False
+                else:
+                    is_found = True 
+                    status=False
+                report.update({column:{
+                    "p_value":float(is_same_dist.pvalue),
+                    "drift_status":is_found
+                    
+                    }})
+            
+            drift_report_file_path = self.data_validation_config.drift_report_file_path
+            
+            #Create directory
+            dir_path = os.path.dirname(drift_report_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            write_yaml_file(file_path=drift_report_file_path,content=report,)
+            return status
+        except Exception as e:
+            raise SensorException(e,sys)
+   
+
     def initiate_data_validation(self)->DataValidationArtifact:
-        try :
+        try:
             error_message = ""
             train_file_path = self.data_ingestion_artifact.trained_file_path
             test_file_path = self.data_ingestion_artifact.test_file_path
 
-            #reading the data from train and test file location
+            #Reading data from train and test file location
             train_dataframe = DataValidation.read_data(train_file_path)
             test_dataframe = DataValidation.read_data(test_file_path)
 
-            #validate number of columns
-            status = self.validate_number_of_columns(dataframe= train_dataframe)
+            #Validate number of columns
+            status = self.validate_number_of_columns(dataframe=train_dataframe)
             if not status:
-                error_message = f"{error_message} train dataframe does not contain all columns"
-
-            status = self.validate_number_of_columns(dataframe = test_dataframe)
+                error_message=f"{error_message}Train dataframe does not contain all columns.\n"
+            status = self.validate_number_of_columns(dataframe=test_dataframe)
             if not status:
-                error_message = f"{error_message} test dataframe does not contain all columns"
+                error_message=f"{error_message}Test dataframe does not contain all columns.\n"
+        
 
-            #validate numerical columns
-            status = self.is_numerical_column_exist(dataframe = train_dataframe)
+            #Validate numerical columns
+
+            status = self.is_numerical_column_exist(dataframe=train_dataframe)
             if not status:
-                error_message =f"{error_message} train dataframe does not contain all numerical columns"
-
-            status = self.is_numerical_column_exist(dataframe = test_dataframe)
+                error_message=f"{error_message}Train dataframe does not contain all numerical columns.\n"
+            
+            status = self.is_numerical_column_exist(dataframe=test_dataframe)
             if not status:
-                error_message =f"{error_message} test dataframe does not contain all numerical columns"
-
+                error_message=f"{error_message}Test dataframe does not contain all numerical columns.\n"
+            
             if len(error_message)>0:
                 raise Exception(error_message)
 
-            #let's check for the data drift
-            return None
-           
-        except Exception as e :
+            #Let check data drift
+            status = self.detect_dataset_drift(base_df=train_dataframe,current_df=test_dataframe)
+
+            data_validation_artifact = DataValidationArtifact(
+                validation_status=status,
+                valid_train_file_path=self.data_ingestion_artifact.trained_file_path,
+                valid_test_file_path=self.data_ingestion_artifact.test_file_path,
+                invalid_train_file_path=None,
+                invalid_test_file_path=None,
+                drift_report_file_path=self.data_validation_config.drift_report_file_path,
+            )
+
+            return data_validation_artifact
+        except Exception as e:
             raise SensorException(e,sys)
