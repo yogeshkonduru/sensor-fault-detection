1f42d283c45d2a808058b4474a4b9045fc59c01c
yogesh konduru <yogeshkonduru@gmail.com>
2022-11-03 21:18:08 +0530

    added data transformation config

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -52,3 +52,29 @@ class DataValidationConfig:
             self.data_validation_dir,
             training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
             training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)
+
+class DataTransformationConfig:
+    def __init__(self,training_pipeline_config: TrainingPipelineConfig):
+        
+        #The file path for the data tranformation diretory 
+        self.data_transformation_dir:str = os.path.join(training_pipeline_config.artifact_dir,
+            training_pipeline.DATA_TRANSFORMATION_DIR_NAME
+         )
+
+         #The file path for the tranformed train file
+        self.transformed_train_file_path:str = os.path.join(self.data_transformation_dir,
+            training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TRAIN_FILE_NAME.replace('csv', 'npy')
+        )
+
+        #The file path for the transformed test file
+        self.transformed_test_file_path:str = os.path.join(self.data_transformation_dir,
+            training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TEST_FILE_NAME.replace('csv', 'npy')
+        )
+
+        #The file path for the transformed object file
+        self.transformed_object_file_path = os.path.join(self.data_transformation_dir,
+            training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
+            training_pipeline.PREPROCSSING_OBJECT_FILE_NAME
+        )
