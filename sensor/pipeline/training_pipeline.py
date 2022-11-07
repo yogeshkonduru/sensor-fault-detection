3997c4d9ccf8ca84e652159dc646d931772e7bea
yogesh konduru <yogeshkonduru@gmail.com>
2022-11-07 21:02:25 +0530

    added s3 bucket to code

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -99,15 +99,15 @@ class TrainPipeline:
 
     def sync_artifact_dir_to_s3(self):
         try:
-            aws_buket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
-            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_buket_url=aws_buket_url)
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_bucket_url=aws_bucket_url)
         except Exception as e:
             raise SensorException(e,sys)
 
     def sync_saved_model_dir_to_s3(self):
         try:
-            aws_buket_url = f"s3://{TRAINING_BUCKET_NAME}/{SAVED_MODEL_DIR}"
-            self.s3_sync.sync_folder_to_s3(folder = SAVED_MODEL_DIR,aws_buket_url=aws_buket_url)
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/{SAVED_MODEL_DIR}"
+            self.s3_sync.sync_folder_to_s3(folder = SAVED_MODEL_DIR,aws_bucket_url=aws_bucket_url)
         except Exception as e:
             raise SensorException(e,sys)
 
@@ -124,8 +124,8 @@ class TrainPipeline:
                 raise Exception("Trained model is not better than the best model")
             model_pusher_arifact = self.start_model_pusher(model_evaluation_artifact = model_evaluation_artifact)
             TrainPipeline.is_pipeline_running=False
-            self.sync_artfact_dir_to_s3()
+            self.sync_artifact_dir_to_s3()
             self.sync_saved_model_dir_to_s3()
         except Exception as e:
-            self.sync_artfact_dir_to_s3()
+            self.sync_artifact_dir_to_s3()
             raise SensorException(e,sys)
